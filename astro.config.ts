ec7338dd3f6508ba6b92ccd6fccda098c4ac8303
Hrihorii Ilin <devopsick@pm.me>
2024-08-14 20:43:52 +0000

    finish

--- astro.config.ts
+++ astro.config.ts
@@ -1,96 +1,75 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import starlight from '@astrojs/starlight'
-import { SITE } from './src/site.config.ts'
-import tailwindcss from '@tailwindcss/vite'
-import rehypeExternalLinks from 'rehype-external-links'
-import mdx from '@astrojs/mdx'
-import { remarkReadingTime } from './src/lib/readTime.ts'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypePrettyCode from 'rehype-pretty-code';
-import rehypeSlug from 'rehype-slug';
-import remarkBreaks from 'remark-breaks';
-import remarkGfm from 'remark-gfm';
+import starlight from '@astrojs/starlight';
+import { SITE } from './src/site.config.ts';
+import tailwindcss from '@tailwindcss/vite';
+import rehypeExternalLinks from 'rehype-external-links';
+import mdx from '@astrojs/mdx';
+import { remarkReadingTime } from './src/lib/readTime';
+import AutoImport from 'unplugin-auto-import/astro'
 
+// https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+        cssMinify: 'lightningcss'
     },
-    site: SITE,
-	prefetch: {
-		defaultStrategy: 'viewport',
-		prefetchAll: true
-	},
-  integrations: [starlight({
+  },
+  site: SITE,
+  prefetch: {
+    defaultStrategy: 'viewport',
+    prefetchAll: true
+  },
+  integrations: [
+    AutoImport({
+        defaultExportByFilename: false,
+        include: [
+            /\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
+        ],
+        packagePresets: ['detect-browser-es'],
+        imports: ['react', 'react-router'],
+        dirs: ['./src/utils/*.ts', './src/hooks'],
+        dts: './src/auto-imports.d.ts'
+    }),
+    starlight({
     title: 'My Docs',
     social: {
-      github: 'https://github.com/withastro/starlight',
+      github: 'https://github.com/withastro/starlight'
     },
     components: {
-        Head: './src/components/layout/Head.astro'
-      },
+      Head: './src/components/layout/Head.astro'
+    },
     customCss: ['./src/styles/globals.css', './src/styles/headings.css'],
-    sidebar: [
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
       {
-        label: 'Guides',
-        items: [
-          // Each item here is one entry in the navigation menu.
-          { label: 'Example Guide', slug: 'guides/example' },
-        ],
-      },
-      {
-        label: 'Reference',
-        autogenerate: { directory: 'reference' },
-      },
-    ],
-  }), react(), mdx({
-    syntaxHighlight: false,
-    remarkPlugins: [remarkGfm, remarkBreaks],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: 'wrap',
-          properties: {
-            className: ['anchor'],
-          },
-        },
-      ],
-      [
-        rehypeExternalLinks,
-        {
-          properties: {
-            class: 'external-link',
-          },
-          target: '_blank',
-          rel: ['noopener noreferrer'],
-        },
-      ],
-      [
-        rehypePrettyCode,
-      ],
-    ],
-  }),],
+        label: 'Example Guide',
+        slug: 'guides/example'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), react(), mdx()],
   markdown: {
-    rehypePlugins: [
-        [
-            rehypeExternalLinks,
-            {
-                content: {
-                    type: 'text',
-                    value: ' 🔗'
-                },
-                target: '_blank',
-                rel: ['nofollow', 'noreferrer']
-            }
-        ]
-    ],
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      },
+      target: '_blank',
+      rel: ['nofollow', 'noreferrer']
+    }]],
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
-        theme: 'github-dark-dimmed',
-        wrap: true
+      theme: 'github-dark-dimmed',
+      wrap: true
     },
     gfm: true
-}
-})
+  }
+});
